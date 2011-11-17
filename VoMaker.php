5faa87db358cfda2bb5e78dc792eaada1bcbf35c
Your Name <iannsp@gmail.com>
2011-11-17 19:19:06 +0000

    transfer responsability to Driver and act like a proxy

--- VoMaker.php
+++ VoMaker.php
@@ -1,41 +1,18 @@
 <?php
+namespace VoMaker;
+use VoMaker\Driver\PgSql;
+use VoMaker\Driver\Mysql;
 
 class VoMaker{
-	private  $C;
-	public function __construct($dsn, $user, $password){
-		try{
-			$this->C = new PDO($dsn,$user, $password);
-		}catch(Exception $E){
-			throw new Exception("Cannot connet to {$dsn}");
-		}
+	private $cmd = Array();
+	private $driver ;
+	public function __construct(\PDO $objectData, array $cmd){
+			$this->cmd		= $cmd;
+			$this->driver = New $cmd['driver']($objectData);
 	}
-	public function listTable(){
-		
-		$stmt = $this->C->query("show tables");
-		$dados = $stmt->fetchAll();
-		$retorno = Array();
-		foreach ($dados as $idx =>$d){
-			array_push( $retorno, $d[0]);
-		}
-		return $retorno;
-	}
-	public function MakeVO($tbl, $path){
-		$stmt = $this->C->query("show fields from {$tbl}");
-		$dados = $stmt->fetchAll();
-		$file = fopen($path."{$tbl}.php", "w");
-		fwrite($file, "<?php \nClass {$tbl}{\n");
-		foreach ($dados as $data){
-			fwrite($file, "    /**\n    @property {$data['Type']} {$data['Extra']} {$data['Key']};\n    */\n");
-			fwrite($file, "    public \${$data['Field']};\n");
-		}
-		fwrite($file, "\n}\n?>");
-	}
-	public function MakeAllVO($path){
-		$tbls = $this->listTable();
-		foreach ($tbls as $tbl){
-			$this->MakeVO($tbl, $path);
-		}
+	public function exec()
+	{
+		$cmd = $this->cmd['cmd'];
+		return $this->driver->$cmd($this->cmd);
 	}
 }
-
-?>
