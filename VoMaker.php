d984cce2113ba0581ec9749ad7db3845ad209faf
Ivo Nascimento <iannsp@gmail.com>
2011-11-18 15:56:05 -0200

    add create using Template Class

--- VoMaker.php
+++ VoMaker.php
@@ -17,9 +17,22 @@ class VoMaker{
         return $this->$cmd();
     }
     private function listtable(){
-        return $this->driver->listtables($this->cmd['tables']);
+        return $this->driver->listTable($this->cmd['tables']);
     }
     private function create(){
-        return $this->driver->create($this->cmd['tables'], $this->cmd['output']);
+        $tmpl = New \VoMaker\Template($this->cmd['template']);
+        if ($this->cmd['tables'] =='ALLTABLES')
+            $theTables = $this->listTable();
+        else
+            $theTables  = explode(',', $this->cmd['tables']);
+        foreach ($theTables as $tablename) {
+            $fields     = $this->driver->create($tablename);
+            $index = array_shift($fields);
+            foreach ($fields as $field) {
+                $tmpl->addField($field);
+            }
+            $tmpl->setIndex(implode(',',$index ));
+            $tmpl->create($tablename, $this->cmd['output']);
+        }
     }
 }
