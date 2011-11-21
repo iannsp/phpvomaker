fdc35a89fcf27a05706cebf1645b852551b99607
Your Name <iannsp@gmail.com>
2011-11-21 14:53:52 +0000

    fix the outuput to output variable

--- Template.php
+++ Template.php
@@ -10,15 +10,15 @@ class Template{
         if (is_null($templatename)?"vo":$templatename);
         $this->template = file_get_contents(__DIR__."/template/{$templatename}");
     }
-    public function create($voname, $outuput)
+    public function create($voname, $output)
     {
-        $output = (is_null($output)?"./":$outuput);
+        $output = (is_null($output)?"./":$output);
         $template = $this->template;
         $template = str_replace('[fields]',implode("\n",$this->txtfield), $template);
         $template = str_replace('[voname]',ucfirst($voname), $template);
         $template = str_replace('[tablename]',$voname, $template);
         $template = str_replace('[index]',$this->indexName, $template);
-        $file = fopen($outuput."/".ucfirst($voname).".php", "w");
+        $file = fopen($output."/".ucfirst($voname).".php", "w");
         fwrite($file, $template);
         fclose($file);
     }
@@ -27,13 +27,13 @@ class Template{
     }
     public function addField(\StdClass $field)
     {
-        array_push($this->txtfield, 
+        array_push($this->txtfield,
         "\t/**
 \t* @name {$field->name}
 \t* @type {$field->type}
 \t* @acceptnull {$field->acceptnull}
 \t* @primarykey ".(($field->pk)?"1":"0")."\n\t**/
 \tpublic \${$field->name};\n");
-        
+
     }
 }
